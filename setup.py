76fe180a9ddfb4b280abfee9b0a482d1ac914064
Ben Skinner <ben.vochsel@gmail.com>
2023-11-03 15:30:19 +1100

    chore: changed setuptool

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-import setuptools
+from distutils.core import setup
 from magpai.version import Version
 
 
-setuptools.setup(name='magpai',
-                 version=Version('1.0.1').number,
-                 description='Magpai python bindings',
-                 long_description=open('README.md').read().strip(),
-                 author='Package Author',
-                 author_email='ben@magpai.app',
-                 url='http://magpai.app',
-                 py_modules=['magpai'],
-                 install_requires=[],
-                 license='MIT License',
-                 zip_safe=False,
-                 keywords='magpai ai workflows',
-                 classifiers=['Packages', 'Boilerplate'])
+setup(name='magpai',
+    version=Version('1.0.1').number,
+    description='Magpai python bindings',
+    long_description=open('README.md').read().strip(),
+    author='Package Author',
+    author_email='ben@magpai.app',
+    url='http://magpai.app',
+    py_modules=['magpai'],
+    install_requires=[],
+    license='MIT License',
+    zip_safe=False,
+    keywords='magpai ai workflows',
+    classifiers=['Packages', 'Boilerplate'])
